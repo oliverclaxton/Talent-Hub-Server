5b90ecc3f09fb554e0b3186c2a2996336f03afcb
oliverclaxton <oliver.claxton66@gmail.com>
2020-11-23 17:19:05 +0100

    built skeleton of express app

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const app = express();
+
+const loggerMiddleWare = require("morgan");
+app.use(loggerMiddleWare("dev"));
+
+const bodyParserMiddleWare = express.json();
+app.use(bodyParserMiddleWare);
+
+const corsMiddleWare = require("cors");
+app.use(corsMiddleWare());
+
+const authMiddleWare = require("./auth/middleware");
+// const authRouter = require("./routers/auth");
+
+// GET endpoint for testing purposes, can be removed
+app.get("/", (req, res) => {
+  res.send("Hi from express");
+});
+
+// POST endpoint for testing purposes, can be removed
+app.post("/echo", (req, res) => {
+  res.json({
+    youPosted: {
+      ...req.body,
+    },
+  });
+});
+
+// POST endpoint which requires a token for testing purposes, can be removed
+app.post("/authorized_post_request", authMiddleWare, (req, res) => {
+  // accessing user that was added to req by the auth middleware
+  const user = req.user;
+  // don't send back the password hash
+  delete user.dataValues["password"];
+
+  res.json({
+    youPosted: {
+      ...req.body,
+    },
+    userFoundWithToken: {
+      ...user.dataValues,
+    },
+  });
+});
+
+// Listen for connections on specified port (default is port 4000)
+const { PORT } = require("./config/constants");
+
+app.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}`);
+});
