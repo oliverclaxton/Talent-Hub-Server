7549073be3879b1e6f78d406701853a4ad11f9ce
oliverclaxton <oliver.claxton66@gmail.com>
2020-12-01 13:29:11 +0100

    added more campaigns to seeders

--- seeders/20201124092409-some-campaigns.js
+++ seeders/20201124092409-some-campaigns.js
@@ -44,6 +44,45 @@ module.exports = {
           createdAt: new Date(),
           updatedAt: new Date(),
         },
+        {
+          title: "Byredo",
+          description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam praesentium dolores blanditiis distinctio odit unde, id ipsam sapiente tempore vero.",
+          dueDate: new Date("2020-12-25"),
+          briefLink:
+            "https://docs.google.com/presentation/d/1mOrY-eN7KXpAmtZvVR_rC2wFdL6z6qPQHHlUHVjMxA8/edit?usp=sharing",
+          contractLink:
+            "https://docs.google.com/document/d/1VS55_0e9CBOTBsp7P2j0JauA99xbQxkFuaibTeOIfwk/edit?usp=sharing",
+          statusId: 2,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          title: "Toteme",
+          description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam praesentium dolores blanditiis distinctio odit unde, id ipsam sapiente tempore vero.",
+          dueDate: new Date("2020-12-15"),
+          briefLink:
+            "https://docs.google.com/presentation/d/1mOrY-eN7KXpAmtZvVR_rC2wFdL6z6qPQHHlUHVjMxA8/edit?usp=sharing",
+          contractLink:
+            "https://docs.google.com/document/d/1VS55_0e9CBOTBsp7P2j0JauA99xbQxkFuaibTeOIfwk/edit?usp=sharing",
+          statusId: 3,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          title: "Fillipa K",
+          description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam praesentium dolores blanditiis distinctio odit unde, id ipsam sapiente tempore vero.",
+          dueDate: new Date("2020-12-30"),
+          briefLink:
+            "https://docs.google.com/presentation/d/1mOrY-eN7KXpAmtZvVR_rC2wFdL6z6qPQHHlUHVjMxA8/edit?usp=sharing",
+          contractLink:
+            "https://docs.google.com/document/d/1VS55_0e9CBOTBsp7P2j0JauA99xbQxkFuaibTeOIfwk/edit?usp=sharing",
+          statusId: 1,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
       ],
       {}
     );
--- seeders/20201124095641-some-campaignImages.js
+++ seeders/20201124095641-some-campaignImages.js
@@ -7,7 +7,7 @@ module.exports = {
       [
         {
           imageUrl:
-            "http://res.cloudinary.com/drzludyk5/image/upload/v1606466896/lj0vwret3ahylectfpnj.jpg",
+            "http://res.cloudinary.com/drzludyk5/image/upload/v1606811074/qpcpl663jfvrzj3bwmqd.jpg",
           caption: "here is a image of me infront of nothing ",
           campaignId: 1,
           userId: 2,
@@ -16,7 +16,43 @@ module.exports = {
         },
         {
           imageUrl:
-            "http://res.cloudinary.com/drzludyk5/image/upload/v1606466896/lj0vwret3ahylectfpnj.jpg",
+            "http://res.cloudinary.com/drzludyk5/image/upload/v1606811090/t3bz9unbhvhkyzfjapxh.jpg",
+          caption: "here is a image of me infront of nothing ",
+          campaignId: 1,
+          userId: 2,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          imageUrl:
+            "http://res.cloudinary.com/drzludyk5/image/upload/v1606811200/ijwexlv6lvftc6ykw6mo.jpg",
+          caption: "here is a image of me infront of nothing ",
+          campaignId: 1,
+          userId: 2,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          imageUrl:
+            "http://res.cloudinary.com/drzludyk5/image/upload/v1606811218/mgbmgbaxm7kp28qum3tw.jpg",
+          caption: "here is a image of me infront of nothing ",
+          campaignId: 1,
+          userId: 2,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          imageUrl:
+            "http://res.cloudinary.com/drzludyk5/image/upload/v1606811394/eerymqephjomhvdhc2uy.jpg",
+          caption: "here is a image of me infront of nothing ",
+          campaignId: 1,
+          userId: 2,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          imageUrl:
+            "http://res.cloudinary.com/drzludyk5/image/upload/v1606811416/phgxsxtqnsxavf3eghvm.jpg",
           caption: "here is a image of me infront of nothing ",
           campaignId: 1,
           userId: 2,
