03ca299ba0812d3b2d6e824355bdfeb778687eb5
oliverclaxton <oliver.claxton66@gmail.com>
2020-12-01 15:24:49 +0100

    changed image caption seeds

--- seeders/20201124095641-some-campaignImages.js
+++ seeders/20201124095641-some-campaignImages.js
@@ -8,7 +8,8 @@ module.exports = {
         {
           imageUrl:
             "http://res.cloudinary.com/drzludyk5/image/upload/v1606811074/qpcpl663jfvrzj3bwmqd.jpg",
-          caption: "here is a image of me infront of nothing ",
+          caption:
+            "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam enim vitae praesentium accusamus dicta deleniti tempore totam dolorum veritatis eius?",
           campaignId: 1,
           userId: 2,
           createdAt: new Date(),
@@ -17,7 +18,8 @@ module.exports = {
         {
           imageUrl:
             "http://res.cloudinary.com/drzludyk5/image/upload/v1606811090/t3bz9unbhvhkyzfjapxh.jpg",
-          caption: "here is a image of me infront of nothing ",
+          caption:
+            "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam enim vitae praesentium accusamus dicta deleniti tempore totam dolorum veritatis eius?",
           campaignId: 1,
           userId: 2,
           createdAt: new Date(),
@@ -26,7 +28,8 @@ module.exports = {
         {
           imageUrl:
             "http://res.cloudinary.com/drzludyk5/image/upload/v1606811200/ijwexlv6lvftc6ykw6mo.jpg",
-          caption: "here is a image of me infront of nothing ",
+          caption:
+            "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam enim vitae praesentium accusamus dicta deleniti tempore totam dolorum veritatis eius?",
           campaignId: 1,
           userId: 2,
           createdAt: new Date(),
@@ -35,7 +38,8 @@ module.exports = {
         {
           imageUrl:
             "http://res.cloudinary.com/drzludyk5/image/upload/v1606811218/mgbmgbaxm7kp28qum3tw.jpg",
-          caption: "here is a image of me infront of nothing ",
+          caption:
+            "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam enim vitae praesentium accusamus dicta deleniti tempore totam dolorum veritatis eius?",
           campaignId: 1,
           userId: 2,
           createdAt: new Date(),
@@ -44,7 +48,8 @@ module.exports = {
         {
           imageUrl:
             "http://res.cloudinary.com/drzludyk5/image/upload/v1606811394/eerymqephjomhvdhc2uy.jpg",
-          caption: "here is a image of me infront of nothing ",
+          caption:
+            "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam enim vitae praesentium accusamus dicta deleniti tempore totam dolorum veritatis eius?",
           campaignId: 1,
           userId: 2,
           createdAt: new Date(),
@@ -53,7 +58,8 @@ module.exports = {
         {
           imageUrl:
             "http://res.cloudinary.com/drzludyk5/image/upload/v1606811416/phgxsxtqnsxavf3eghvm.jpg",
-          caption: "here is a image of me infront of nothing ",
+          caption:
+            "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam enim vitae praesentium accusamus dicta deleniti tempore totam dolorum veritatis eius?",
           campaignId: 1,
           userId: 2,
           createdAt: new Date(),
@@ -62,7 +68,8 @@ module.exports = {
         {
           imageUrl:
             "http://res.cloudinary.com/drzludyk5/image/upload/v1606466896/lj0vwret3ahylectfpnj.jpg",
-          caption: "here is a image of me infront of nothing ",
+          caption:
+            "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam enim vitae praesentium accusamus dicta deleniti tempore totam dolorum veritatis eius?",
           campaignId: 2,
           userId: 3,
           createdAt: new Date(),
@@ -71,7 +78,8 @@ module.exports = {
         {
           imageUrl:
             "http://res.cloudinary.com/drzludyk5/image/upload/v1606466896/lj0vwret3ahylectfpnj.jpg",
-          caption: "here is a image of me infront of nothing ",
+          caption:
+            "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam enim vitae praesentium accusamus dicta deleniti tempore totam dolorum veritatis eius?",
           campaignId: 2,
           userId: 2,
           createdAt: new Date(),
@@ -80,7 +88,8 @@ module.exports = {
         {
           imageUrl:
             "http://res.cloudinary.com/drzludyk5/image/upload/v1606466896/lj0vwret3ahylectfpnj.jpg",
-          caption: "here is a image of me infront of nothing ",
+          caption:
+            "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam enim vitae praesentium accusamus dicta deleniti tempore totam dolorum veritatis eius?",
           campaignId: 3,
           userId: 3,
           createdAt: new Date(),
@@ -89,7 +98,8 @@ module.exports = {
         {
           imageUrl:
             "http://res.cloudinary.com/drzludyk5/image/upload/v1606466896/lj0vwret3ahylectfpnj.jpg",
-          caption: "here is a image of me infront of nothing ",
+          caption:
+            "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam enim vitae praesentium accusamus dicta deleniti tempore totam dolorum veritatis eius?",
           campaignId: 3,
           userId: 3,
           createdAt: new Date(),
